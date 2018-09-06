31c5bbbf6acd7b6dabf7ab7de203a69f120ef7f1
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-06 03:58:33 +0100

    no message

--- miners/claymore-eth-v11.9/start_mining.sh
+++ miners/claymore-eth-v11.9/start_mining.sh
@@ -2,6 +2,9 @@
 
 location=usa
 server=stratum+tcp://cryptonight.$location.nicehash.com:3355
+
+echo $server
+
 # nanopool
 # sudo nohup ./ethdcrminer64 -epool eth-us-west1.nanopool.org:9999 -ewal 0x71e377d4b4125e3c548fb8fe4c06db63f710fbc8.i216368115/admin@deltacolo.com -epsw x -mode 1 -ftime 10 > /mcp/logs/miner.log & 
 
