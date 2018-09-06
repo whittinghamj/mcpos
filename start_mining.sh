433dedd64a3315fcddfd5a22b8fb53f766a6283e
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-06 02:28:38 +0100

    no message

--- start_mining.sh
+++ start_mining.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /mcp/miners/claymore-eth-v11.9
-sudo nohup ethdcrminer64 -epool eth-us-west1.nanopool.org:9999 -ewal 0x71e377d4b4125e3c548fb8fe4c06db63f710fbc8.worker_1/admin@deltacolo.com -epsw x -mode 1 -ftime 10 > /mcp/logs/miner.log & 
+sudo nohup ./ethdcrminer64 -epool eth-us-west1.nanopool.org:9999 -ewal 0x71e377d4b4125e3c548fb8fe4c06db63f710fbc8.worker_1/admin@deltacolo.com -epsw x -mode 1 -ftime 10 > /mcp/logs/miner.log & 
