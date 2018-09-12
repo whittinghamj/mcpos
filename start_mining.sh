c3f50dfd9fe1a7905e3271ab4751b83c6f6b8ebf
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-12 09:37:40 +0100

    no message

--- start_mining.sh
+++ start_mining.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 
+# pid / lock file
+mypidfile=/mcp/start_mining.pid
+trap "rm -f -- '$mypidfile'" EXIT
+echo $$ > "$mypidfile"
+
+
+# nvidia vars
 export GPU_FORCE_64BIT_PTR=0
 export GPU_MAX_HEAP_SIZE=100
 export DISPLAY=:0
@@ -10,6 +17,6 @@ export GPU_USE_SYNC_OBJECTS=0
 export GPU_SINGLE_ALLOC_PERCENT=100
 
 
-# nicehash daggerhash + eth
+# miner
 cd /mcp/miners/claymore-eth-v11.9
 sudo nohup ./ethdcrminer64 -epool stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 -ewal 33Z1aVUDJxofRz2QxvjkFnfqtLPifc2nWN.mcpdevus -epsw x -esm 3 -allpools 1 -estale 0 -dpool stratum+tcp://decred.eu.nicehash.com:3354 -dwal 33Z1aVUDJxofRz2QxvjkFnfqtLPifc2nWN.mcpdevus > /mcp/logs/miner.log & 
