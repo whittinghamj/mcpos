538c16b0349c73463a4c33a4bd2fced5d6a6b0c8
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-12 01:29:02 +0100

    no message

--- nvidia.sh
+++ nvidia.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-echo "Configuring NVIDIA GPUs"
-echo " "
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #####
 # Global Settings
 #####
@@ -176,46 +187,7 @@ else
 	+===========================================================+
 	EOF
 
+	read -p "Press any key to reboot... "
+
 	sudo reboot
 fi
-
-while [[ $# -gt 0 ]]; do
-
-	key="$1"
-
-	case $key in
-	esac
-
-	shift
-done
-
-#####
-# Compute Input
-#####
-
-#####
-# Validate Input
-#####
-
-#####
-# Mining Rig Setup
-#####
-
-# setup SSH identity
-[ -e ~/.ssh/id_rsa ] || ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
-
-# install tools we'll use later
-sudo apt-get install -y jq curl openssh-server openssh-client ubuntu-drivers-common
-
-# remove other useless things
-sudo apt-get -y autoremove
-
-# upgrade what we can
-sudo apt-get -y upgrade
-
-# 62 wide, 60 usable, 58 used
-cat << EOF
-+===========================================================+
-| Mining Rig Configured                                     |
-+===========================================================+
-EOF
