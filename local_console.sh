cd8051dc72684c7c655c1f9f7d387ed3686639c4
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-13 20:34:45 +0100

    no message

--- local_console.sh
+++ local_console.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-DARKGRAY='\033[1;30m'
-RED='\033[0;31m'    
-LIGHTRED='\033[1;31m'
-GREEN='\033[0;32m'    
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'    
-PURPLE='\033[0;35m'    
-LIGHTPURPLE='\033[1;35m'
-CYAN='\033[0;36m'    
-WHITE='\033[1;37m'
-SET='\033[0m'
-
 HASHRATE="$(sh /mcp/stats.sh)";
 UPTIME="$(uptime | sed -E 's/^[^,]*up *//; s/, *[[:digit:]]* users.*//; s/min/minutes/; s/([[:digit:]]+):0?([[:digit:]]+)/\1 hours, \2 minutes/' )";
 
@@ -21,11 +9,13 @@ echo "System Uptime: $UPTIME"
 echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then
-    echo "Internet Connection: ${GREEN}Online${SET}"
+    echo "Internet Connection: Online"
 else
-    echo "Internet Connection: ${RED}Offline${SET}"
+    echo "Internet Connection: Offline"
 fi
 
 echo "Miner Hashrate: $HASHRATE"
+
+echo " "
 
 sudo nvidia-smi
