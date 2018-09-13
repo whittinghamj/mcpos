731afa999643711beaf9367078b639293ad4123b
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-13 19:50:47 +0100

    no message

--- boot.sh
+++ boot.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+DARKGRAY='\033[1;30m'
+RED='\033[0;31m'    
+LIGHTRED='\033[1;31m'
+GREEN='\033[0;32m'    
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'    
+PURPLE='\033[0;35m'    
+LIGHTPURPLE='\033[1;35m'
+CYAN='\033[0;36m'    
+WHITE='\033[1;37m'
+SET='\033[0m'
+
+
 rm -rf /mcp/*.loc
 rm -rf /mcp/logs/*
+
 
 touch /mcp/logs/console.log
 touch /mcp/logs/deamon.log
 touch /mcp/logs/miner.log
+
 
 export DISPLAY=:0
 export GPU_MAX_ALLOC_PERCENT=100
@@ -14,4 +29,39 @@ export GPU_SINGLE_ALLOC_PERCENT=100
 export GPU_MAX_HEAP_SIZE=100
 export GPU_FORCE_64BIT_PTR=1
 
-# screen -S miner -X logfile /mcp/logs/miner.log 1>/dev/null 2>/dev/null
+
+echo "[ ${GREEN}OK${SET} ] Loading BIOS modifications."
+sleep 1
+echo "[ ${GREEN}OK${SET} ] Loading firmware packages."
+sleep 1
+echo "[ ${GREEN}OK${SET} ] Configuring firewall."
+sleep 1
+echo "[ ${GREEN}OK${SET} ] Booting MCP OS."
+sleep 1
+echo "[ ${GREEN}OK${SET} ] Updating MCP OS."
+sleep 1
+echo "[ ${GREEN}OK${SET} ] Configuring MCP OS."
+sleep 1
+echo "[ ${GREEN}OK${SET} ] Detecting installed GPUs."
+sleep 1
+
+
+NVIDIA=`lspci | grep VGA | grep NVIDIA | wc -l`
+ATI=`lspci | grep VGA | grep ATI | wc -l`
+
+if [ "$NVIDIA" -gt "0" ]; then
+      echo "[ ${GREEN}OK${SET} ] NVIDIA GPUs found."
+      sleep 1
+      echo "[ ${GREEN}OK${SET} ] Loading monitors."
+
+      sleep 5
+      sudo sh /mcp/local_console.sh
+      # sudo watch -n1 nvidia-smi
+      exit 1
+fi
+
+if [ "$NVIDIA" -gt "0" ]; then
+      echo "[ ${GREEN}OK${SET} ] ATI GPUs found."
+      sleep 1
+      exit 1
+fi
--- local_console.sh
+++ local_console.sh
@@ -1,37 +1,7 @@
 #!/bin/bash
 
-echo 'The system is booting, please stand by.'
-sleep 2
-echo " "
+HASHRATE="$(sh /mcp/stats.sh)";
 
-echo "#########################################"
-echo "Checking connection to Internet..."
-i=0
-while [ "$i" -le 6 ]; do
-  # timeout 10 works about 20 seconds :)
-  CZY=`host -W 10 -t SOA google.com | grep -ci "google.com"`
-  if [ "$CZY" -gt 0 ]; then
-    echo "Internet OK."
-    break
-  fi
-  i=$((i+1))
-  echo "Probing"
-done
-echo "#########################################"
-sleep 2
-echo " "
+echo "Hashrate: $HASHRATE"
 
-
-NVIDIA=`lspci | grep VGA | grep NVIDIA | wc -l`
-ATI=`lspci | grep VGA | grep ATI | wc -l`
-
-if [ "$NVIDIA" -gt "0" ]; then
-	echo 'NVIDIA GPUs detected, Loading console monitor.'
-	sleep 5
-    sudo watch -n1 nvidia-smi
-    exit 1
-fi
-
-if [ "$NVIDIA" -gt "0" ]; then
-    echo 'ATI GPUs detected.'
-fi
+sudo nvidia-smi
