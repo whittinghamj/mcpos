d0eb5305ecb19170b5fd1ea6c46824473914a09c
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-13 16:44:16 +0100

    no message

--- local_console.sh
+++ local_console.sh
@@ -3,6 +3,6 @@
 NVIDIA=`lspci | grep VGA | grep NVIDIA | wc -l`
 ATI=`lspci | grep VGA | grep ATI | wc -l`
 
-if (( $NVIDIA > 0 )); then
-    nvidia-smi
+if [ "$NVIDIA" -gt "0" ]; then
+    sudo nvidia-smi
 fi
