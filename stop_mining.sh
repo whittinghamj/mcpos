c130ee83ed8a5995a0bd7799a99ae3c8dbad8eef
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-13 17:36:11 +0100

    no message

--- stop_mining.sh
+++ stop_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-COUNT=`ps aux | grep '/mcp/miners' | awk '{print $2}' | wc -l`
-COUNT=$COUNT + 1
+TOTAL=`ps aux | grep '/mcp/miners' | awk '{print $2}' | wc -l`
+TOTAL=TOTAL + 1
 sudo kill $(ps aux | grep '/mcp/miners' | awk '{print $2}') > /dev/null 2>&1
 sudo kill $(ps aux | grep 'start_mining.sh' | awk '{print $2}') > /dev/null 2>&1
 
