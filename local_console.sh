663bb579360f39586e5b9c46c8857c070989f5af
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-13 16:42:25 +0100

    no message

--- local_console.sh
+++ local_console.sh
@@ -3,12 +3,6 @@
 NVIDIA=`lspci | grep VGA | grep NVIDIA | wc -l`
 ATI=`lspci | grep VGA | grep ATI | wc -l`
 
-if[ $NVIDIA > 0];
-then
-	echo 'NVIDIA DETECTED'
-	sleep 5
-	nvidia-smi
-	exit 1
-else
-
+if (( $NVIDIA > 0 )); then
+    nvidia-smi
 fi
