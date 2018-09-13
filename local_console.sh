8007e573da14184b8fe660527f0553fa8de9445f
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-13 16:47:01 +0100

    no message

--- local_console.sh
+++ local_console.sh
@@ -4,5 +4,7 @@ NVIDIA=`lspci | grep VGA | grep NVIDIA | wc -l`
 ATI=`lspci | grep VGA | grep ATI | wc -l`
 
 if [ "$NVIDIA" -gt "0" ]; then
+	echo 'The system is booting, please stand by.'
+	sleep 5
     sudo watch -n1 nvidia-smi
 fi
