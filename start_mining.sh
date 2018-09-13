141e0c95a4e89ff0881d774f15617dd80ebd2469
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-13 11:50:21 +0100

    no message

--- start_mining.sh
+++ start_mining.sh
@@ -5,7 +5,11 @@
 # pid / lock file
 
 
-# miner
-sudo nohup /mcp/miners/claymore-eth-v11.9/ethdcrminer64 -epool stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 -ewal 33Z1aVUDJxofRz2QxvjkFnfqtLPifc2nWN.mcpdevus -epsw x -esm 3 -allpools 1 -estale 0 -dpool stratum+tcp://decred.eu.nicehash.com:3354 -dwal 33Z1aVUDJxofRz2QxvjkFnfqtLPifc2nWN.mcpdevus > /mcp/logs/miner.log & 
+# claymore-eth-v11.9
+# sudo nohup /mcp/miners/claymore-eth-v11.9/ethdcrminer64 -epool stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 -ewal 33Z1aVUDJxofRz2QxvjkFnfqtLPifc2nWN.mcpdevus -epsw x -esm 3 -allpools 1 -estale 0 -dpool stratum+tcp://decred.eu.nicehash.com:3354 -dwal 33Z1aVUDJxofRz2QxvjkFnfqtLPifc2nWN.mcpdevus > /mcp/logs/miner.log & 
 
+# silentarmy
 # /mcp/miners/silentarmy/silentarmy --use 0 -c stratum+tcp://us1-zcash.flypool.org:3333 -u t1dAGBEwP6jJVqRozRUMTp4EUzeMosAHeQz
+
+# ccminer-tpruvot-v2.3-cuda9.1
+sudo nohup /mcp/miners/ccminer-tpruvot-v2.3-cuda9.1/-a cryptonight -o stratum+tcp://cryptonight.eu.nicehash.com:3355 -u 33Z1aVUDJxofRz2QxvjkFnfqtLPifc2nWN -p x
