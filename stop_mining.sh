00e52866e41caaf2097c3188eb9a75aba945cea1
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-13 17:39:15 +0100

    no message

--- stop_mining.sh
+++ stop_mining.sh
@@ -4,4 +4,4 @@ TOTAL='$TOTAL + 1' | bc
 sudo kill $(ps aux | grep '/mcp/miners' | awk '{print $2}') > /dev/null 2>&1
 sudo kill $(ps aux | grep 'start_mining.sh' | awk '{print $2}') > /dev/null 2>&1
 
-echo 'Terminating $TOTAL mining processes.'
+echo "Terminating $TOTAL mining processes."
