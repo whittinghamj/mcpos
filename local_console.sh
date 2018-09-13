bf68ac04346531387b5ea092ad9b85d720c36fb3
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-13 16:48:57 +0100

    no message

--- local_console.sh
+++ local_console.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+echo 'The system is booting, please stand by.'
+sleep 2
+
 NVIDIA=`lspci | grep VGA | grep NVIDIA | wc -l`
 ATI=`lspci | grep VGA | grep ATI | wc -l`
 
 if [ "$NVIDIA" -gt "0" ]; then
-	echo 'The system is booting, please stand by.'
+	echo 'NVIDIA GPUs detected, Loading console monitor.'
 	sleep 5
     sudo watch -n1 nvidia-smi
+    exit 1
+fi
+
+if [ "$NVIDIA" -gt "0" ]; then
+    echo 'ATI GPUs detected.'
 fi
