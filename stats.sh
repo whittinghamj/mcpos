cb2bfd38f92fb11ef49d0aec746a357b086884c1
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-13 13:33:28 +0100

    no message

--- stats.sh
+++ stats.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # get last 15 lines of the log file
-CONSOLE=`cat /mcp/logs/miner.log | sed 's/\r/g' | grep -a . | tail -n 15 | aha --no-header`
+CONSOLE=`cat /mcp/logs/miner.log | sed 's/\r/\n/g' | grep -a . | tail -n 15 | aha --no-header`
 # replace [space & < > " ' '] with underscore
 CONSOLE=`echo "$CONSOLE" | sed 's/&nbsp;/_/g; s/&amp;/_/g; s/&lt;/_/g; s/&gt;/_/g; s/&quot;/_/g; s/&ldquo;/_/g; s/&rdquo;/_/g;'`
 # remove amp
@@ -14,3 +14,9 @@ CONSOLE_SHORT_PRE=`cat /mcp/logs/miner.log | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g' | 
 echo " "
 
 echo $CONSOLE_SHORT_PRE
+
+CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a " Total " | tail -n 1 | sed -e 's/.*Total \(.*\) Accepted.*/\1/'`
+
+echo " "
+
+echo $CONSOLE_SHORT
