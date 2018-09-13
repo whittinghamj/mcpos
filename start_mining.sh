56d95ed09af85c6fa9463bf2b6a33490f0b660c2
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-13 12:21:47 +0100

    no message

--- start_mining.sh
+++ start_mining.sh
@@ -6,7 +6,10 @@
 
 
 # claymore-eth-v11.9
-sudo nohup /mcp/miners/claymore-eth-v11.9/ethdcrminer64 -epool stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 -ewal 33Z1aVUDJxofRz2QxvjkFnfqtLPifc2nWN.mcpdevus -epsw x -esm 3 -allpools 1 -estale 0 -dpool stratum+tcp://decred.eu.nicehash.com:3354 -dwal 33Z1aVUDJxofRz2QxvjkFnfqtLPifc2nWN.mcpdevus > /mcp/logs/miner.log & 
+# sudo nohup /mcp/miners/claymore-eth-v11.9/ethdcrminer64 -epool stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 -ewal 33Z1aVUDJxofRz2QxvjkFnfqtLPifc2nWN.mcpdevuk -epsw x -esm 3 -allpools 1 -estale 0 -dpool stratum+tcp://decred.eu.nicehash.com:3354 -dwal 33Z1aVUDJxofRz2QxvjkFnfqtLPifc2nWN.mcpdevuk > /mcp/logs/miner.log & 
 
 # xmr-stak
-# sudo nohup /mcp/miners/xmr-stak-v2.4.7-cuda9.1/xmr-stak --currency monero7 -o stratum+tcp://xmr.pool.minergate.com:45560 -u jamie.whittingham@gmail.com -p x
+# sudo nohup /mcp/miners/xmr-stak-v2.4.7-cuda9.1/xmr-stak --currency monero7 -o stratum+tcp://xmr.pool.minergate.com:45560 -u jamie.whittingham@gmail.com -p x > /mcp/logs/miner.log & 
+
+# claymore-zec
+sudo nohup /mcp/miners/claymore-zec/zecminer64 -zpool equihash.eu.nicehash.com:3357 -zwal 33Z1aVUDJxofRz2QxvjkFnfqtLPifc2nWN.mcpdevuk -zpsw x > /mcp/logs/miner.log & 
