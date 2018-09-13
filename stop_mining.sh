624a8ecf05593839382f81fb9b1fd2a8883ed727
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-13 17:34:47 +0100

    no message

--- stop_mining.sh
+++ stop_mining.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+COUNT=`ps aux | grep '/mcp/miners' | awk '{print $2}' | wc -l`
+COUNT=$COUNT + 1
 sudo kill $(ps aux | grep '/mcp/miners' | awk '{print $2}') > /dev/null 2>&1
 sudo kill $(ps aux | grep 'start_mining.sh' | awk '{print $2}') > /dev/null 2>&1
+
+echo 'Terminating $COUNT mining processes.'
