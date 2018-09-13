93ac42f3e2add8353fab0c02a05cc7a465054847
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-13 18:16:10 +0100

    no message

--- check_internet.sh
+++ check_internet.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ok || echo error
+echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    echo "online"
+else
+    echo "offline"
+fi
