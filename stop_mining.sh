b2be0b0b209a7a8055d852f3b408bcd19ff69010
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-13 17:31:25 +0100

    no message

--- stop_mining.sh
+++ stop_mining.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kill $(ps aux | grep '/mcp/miners' | awk '{print $2}') > /dev/null 2>&1
-kill $(ps aux | grep 'start_mining.sh' | awk '{print $2}') > /dev/null 2>&1
+sudo kill $(ps aux | grep '/mcp/miners' | awk '{print $2}') > /dev/null 2>&1
+sudo kill $(ps aux | grep 'start_mining.sh' | awk '{print $2}') > /dev/null 2>&1
