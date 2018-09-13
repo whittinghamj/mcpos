3c3213dd33b66f7db0e049029ffe112b73333257
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-13 16:43:07 +0100

    no message

--- boot.sh
+++ boot.sh
@@ -8,3 +8,5 @@ export GPU_USE_SYNC_OBJECTS=1
 export GPU_SINGLE_ALLOC_PERCENT=100
 export GPU_MAX_HEAP_SIZE=100
 export GPU_FORCE_64BIT_PTR=1
+
+# screen -S miner -X logfile /mcp/logs/miner.log 1>/dev/null 2>/dev/null
