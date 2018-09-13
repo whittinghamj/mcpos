b99ec104d65a90cb583288a5a22330c8f67dccbd
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-13 20:30:36 +0100

    no message

--- local_console.sh
+++ local_console.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 HASHRATE="$(sh /mcp/stats.sh)";
+UPTIME="$(uptime | sed -E 's/^[^,]*up *//; s/, *[[:digit:]]* users.*//; s/min/minutes/; s/([[:digit:]]+):0?([[:digit:]]+)/\1 hours, \2 minutes/' )";
 
+echo "Uptime: $UPTIME"
 echo "Hashrate: $HASHRATE"
 
 sudo nvidia-smi
