09152a3f9a65ba1bcd3588d9cb0d1203d407c907
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-13 11:29:21 +0100

    no message

--- stats.sh
+++ stats.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # get last 15 lines of the log file
-CONSOLE=`cat /var/tmp/screen.miner.log | sed 's/\r/\n/g' | grep -a . | tail -n 15 | aha --no-header`
+CONSOLE=`cat /mcp/logs/miner.log | sed 's/\r/\n/g' | grep -a . | tail -n 15 | aha --no-header`
 # replace [space & < > " ' '] with underscore
 CONSOLE=`echo "$CONSOLE" | sed 's/&nbsp;/_/g; s/&amp;/_/g; s/&lt;/_/g; s/&gt;/_/g; s/&quot;/_/g; s/&ldquo;/_/g; s/&rdquo;/_/g;'`
 # remove amp
