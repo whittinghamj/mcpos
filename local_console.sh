dfef5bd934ffa749a8194a10a56d5ddf0fcf7c44
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-14 03:18:20 +0100

    Hashed out fatel loop.

--- local_console.sh
+++ local_console.sh
@@ -4,7 +4,7 @@ if [ -s /mcp/site_key.txt ]
 then
       HASHRATE="$(sh /mcp/stats.sh)";
 
-      [ -z "$HASHRATE" ] && php -q /mcp/console.php miner_restart > /mcp/logs/miner.log
+      # [ -z "$HASHRATE" ] && php -q /mcp/console.php miner_restart > /mcp/logs/miner.log
 
       UPTIME="$(uptime)";
 
