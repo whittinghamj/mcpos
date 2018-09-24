69e1b368bdbba5a56c7492f483d29a6a101a979d
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-24 21:56:24 +0100

    no message

--- pause_miner.sh
+++ pause_miner.sh
@@ -2,6 +2,7 @@
 
 while /bin/true; do
 	echo 'Miner is in pause mode' >> /mcp/logs/console.log
-    killall -9 miners > /dev/null
+    ## killall -9 miners > /dev/null
+    sudo kill $(ps aux | grep 'miners' | awk '{print $2}') > /dev/null 2>&1
     sleep 5
 done &
