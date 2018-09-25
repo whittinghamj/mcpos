d342cef833e7ac2ef0678a9421aaf0f6825ecfce
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-25 04:44:03 +0100

    no message

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 
-$config_file = file_get_contents('/mcp/miner_config.php', true);
+$config_file = file_get_contents('/mcp/miner_config.php');
 $config_file = json_decode($config_file, true);
 
 echo "Demo Config File \n";
