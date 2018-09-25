99972e5b7551ec6ddc5e018331d7057f0af28095
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-25 04:51:26 +0100

    no message

--- test.php
+++ test.php
@@ -4,16 +4,16 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('error_reporting', E_ALL); 
 
-$config_file = file_get_contents('/mcp/miner_config.php');
+$config_file_raw = file_get_contents('/mcp/miner_config.php');
 
 echo "JSON Array \n";
 
-print_r($config_file);
+print_r($config_file_raw);
 
 echo "\n\n";
 
-$config_file = json_decode($config_file, true);
+$config_file = json_decode($config_file_raw, true);
 
 echo "PHP Array \n";
 
-print_r($config_file, TRUE);
+print_r($config_file);
