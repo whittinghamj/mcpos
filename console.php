89e89bc4a0b375715e9037f8896602f6e68f841b
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-25 04:01:54 +0100

    no message

--- console.php
+++ console.php
@@ -241,11 +241,17 @@ if($task == "miner_jobs")
 					{
 						console_output('Updating Miner Config');
 						console_output($api_url."/api/?key=".$system['api_key']."&c=miner_gpu_get_config&miner_id=".$system['miner_id']);
-						
+
 						// code for updating miner config
 
 						$miner_config_raw = file_get_contents($api_url."/api/?key=".$system['api_key']."&c=miner_gpu_get_config&miner_id=".$system['miner_id']);
 						$miner_config = json_decode($miner_config_raw, true);
+
+						$config_file = '<?php';
+						$config_file .= '';
+						$config_file .= $miner_config;
+
+						file_put_contents('/mcp/miner_config.php', $config_file);
 
 						print_r($miner_config);
 
