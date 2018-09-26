3ab446f26d62072fe622a90a5c82076be4d93d5d
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-26 02:20:03 +0100

    no message

--- stats_test.sh
+++ stats_test.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+JSON_CONFIG=$(cat /mcp/miner_config.php);
+MINER_PATH=`echo "$JSON_CONFIG" | jq -r .gpu_miner_software_folder`
+
+echo $MINER_PATH
+
 # preprocessing
 # remove BASH colors&codes && \r && empty lines && only last 30 lines
 CONSOLE_SHORT_PRE=`cat /mcp/logs/miner.log | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g' | sed 's/\r/\n/g' | grep -a . | tail -n 30`
