f42244fc8f93e8fa5ff0296ac535df3cb2bc558f
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-26 02:52:11 +0100

    no message

--- stats_test.sh
+++ stats_test.sh
@@ -32,33 +32,31 @@ else
 fi
 
 CZY=`echo "$MINER_PATH" | grep -i "bminer" | wc -l`
-#[ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a " Total " | tail -n 1 | awk '{ print $4" "$5 }'`
-[ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a " Total " | tail -n 1 | sed -e 's/.*Total \(.*\) Accepted.*/\1/'`
+# [ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a " Total " | tail -n 1 | sed -e 's/.*Total \(.*\) Accepted.*/\1/'`
 
 ### ccminer-phi-anxmod-216k155
-CZY=`echo "$MINER_PATH" | grep -i "ccminer-phi-anxmod-216k155" | wc -l`
-[ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "[YES]" | tail -n 1 | awk '{ for (i=1;i<=NF;i++)if($i~/H\/s/) print $(i-1)" "$i }'`
+# CZY=`echo "$MINER_PATH" | grep -i "ccminer-phi-anxmod-216k155" | wc -l`
+# [ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "[YES]" | tail -n 1 | awk '{ for (i=1;i<=NF;i++)if($i~/H\/s/) print $(i-1)" "$i }'`
 
 ### ccminer alexis78
-CZY=`echo "$MINER_PATH" | grep -i "ccminer.*alexis78" | wc -l`
-[ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "H/s yes!" | tail -n 1 | awk '{ print $7 }'`
+# CZY=`echo "$MINER_PATH" | grep -i "ccminer.*alexis78" | wc -l`
+# [ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "H/s yes!" | tail -n 1 | awk '{ print $7 }'`
 
 ### ccminer tpruvot | ccminer nevermore brian | ccminer-skunk-krnlx | suprminer
-CZY=`echo "$MINER_PATH" | grep -i "ccminer.*tpruvot\|nevermore\|ccminer-skunk-krnlx\|suprminer" | wc -l`
-[ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "H/s yes!" | tail -n 1 | awk '{ print $(NF-2)" "$(NF-1) }'`
-#[ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "H/s yes!" | tail -n 1 | awk '{ print $7" "$8 }'`
+# CZY=`echo "$MINER_PATH" | grep -i "ccminer.*tpruvot\|nevermore\|ccminer-skunk-krnlx\|suprminer" | wc -l`
+# [ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "H/s yes!" | tail -n 1 | awk '{ print $(NF-2)" "$(NF-1) }'`
 
 ### ccminer KlausT
-CZY=`echo "$MINER_PATH" | grep -i "ccminer.*klaust" | wc -l`
-[ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "H/s yay!" | tail -n 1 | awk '{ print $6" "$7 }'`
+# CZY=`echo "$MINER_PATH" | grep -i "ccminer.*klaust" | wc -l`
+# [ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "H/s yay!" | tail -n 1 | awk '{ print $6" "$7 }'`
 
 ### ccminer Ravencoin
-CZY=`echo "$MINER_PATH" | grep -i "ravencoin" | wc -l`
-[ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "H/s Kraww!" | tail -n 1 | awk '{ print $7" "$8 }'`
+# CZY=`echo "$MINER_PATH" | grep -i "ravencoin" | wc -l`
+# [ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "H/s Kraww!" | tail -n 1 | awk '{ print $7" "$8 }'`
 
 ### ethminer
-CZY=`echo "$MINER_PATH" | grep -i "ethminer" | wc -l`
-[ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a " Speed " | tail -n 1 | awk -F" Speed " '{ print $2 }' | awk '{ print $1" "$2 }'`
+# CZY=`echo "$MINER_PATH" | grep -i "ethminer" | wc -l`
+# [ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a " Speed " | tail -n 1 | awk -F" Speed " '{ print $2 }' | awk '{ print $1" "$2 }'`
 
 #### dstm >=0.6.1
 #CZY=`echo "$MINER_PATH" | grep -i "dstm" | wc -l`
@@ -69,59 +67,59 @@ CZY=`echo "$MINER_PATH" | grep -i "ethminer" | wc -l`
 #[ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a " ========== Sol" | tail -n 1 | awk '{ print $2" "$3 }'`
 
 ### dstm-all
-CZY=`echo "$MINER_PATH" | grep -i "dstm" | wc -l`
-[ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a " ==========" | grep "Sol" | tail -n 1 | awk -F"Sol/s: " '{ print $2 }' | awk '{ print $1" Sols/s" }'`
+# CZY=`echo "$MINER_PATH" | grep -i "dstm" | wc -l`
+# [ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a " ==========" | grep "Sol" | tail -n 1 | awk -F"Sol/s: " '{ print $2 }' | awk '{ print $1" Sols/s" }'`
 
 ### ewbf
-CZY=`echo "$MINER_PATH" | grep -i "ewbf" | wc -l`
-[ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "Total" | tail -n 1 | awk '{ print $4" "$5 }'`
+# CZY=`echo "$MINER_PATH" | grep -i "ewbf" | wc -l`
+# [ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "Total" | tail -n 1 | awk '{ print $4" "$5 }'`
 
 ### sgminer-gm-aceneun | sgminer-brian112358 | sgminer-msvc2015-djm34
-CZY=`echo "$MINER_PATH" | grep -i "sgminer-gm-aceneun\|sgminer-brian112358" | wc -l`
-[ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | sed 's/\r/\n/g' | grep -a "(avg)" | tail -n 1 | awk '{ print $2 }' | sed 's/(avg)://g'`
+# CZY=`echo "$MINER_PATH" | grep -i "sgminer-gm-aceneun\|sgminer-brian112358" | wc -l`
+# [ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | sed 's/\r/\n/g' | grep -a "(avg)" | tail -n 1 | awk '{ print $2 }' | sed 's/(avg)://g'`
 
 ### tdxminer
-CZY=`echo "$MINER_PATH" | grep -i "tdxminer" | wc -l`
-if [ "$CZY" == "1" ]; then
-  CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | sed 's/\r/\n/g' | grep -a "Stats Total" | tail -n 1 | awk '{ print $7 }' | sed 's/^[ \t]*//;s/[ \t]*$//'`
-  if [ "$CONSOLE_SHORT" == "" ]; then
+# CZY=`echo "$MINER_PATH" | grep -i "tdxminer" | wc -l`
+# if [ "$CZY" == "1" ]; then
+  # CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | sed 's/\r/\n/g' | grep -a "Stats Total" | tail -n 1 | awk '{ print $7 }' | sed 's/^[ \t]*//;s/[ \t]*$//'`
+  # if [ "$CONSOLE_SHORT" == "" ]; then
     # brak sekcji Total, ktora wystepuje tylko dla count(GPU)>1
-    CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | sed 's/\r/\n/g' | grep -a "Stats GPU" | tail -n 1 | awk '{ print $8 }'`
-  fi
-fi
+    # CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | sed 's/\r/\n/g' | grep -a "Stats GPU" | tail -n 1 | awk '{ print $8 }'`
+  # fi
+# fi
 
 ### t-rex
-CZY=`echo "$MINER_PATH" | grep -i "t-rex-" | wc -l`
-[ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "\[ OK \]" | tail -n 1 | awk -F "\[ OK \]" '{ print $2 }' | awk '{ print $3" "$4 }'`
+# CZY=`echo "$MINER_PATH" | grep -i "t-rex-" | wc -l`
+# [ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "\[ OK \]" | tail -n 1 | awk -F "\[ OK \]" '{ print $2 }' | awk '{ print $3" "$4 }'`
 
 ### lolminer
-CZY=`echo "$MINER_PATH" | grep -i "lolminer" | wc -l`
-if [ "$CZY" == "1" ]; then
-  CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | sed 's/\r/\n/g' | grep -a "^Average speed" | tail -n 1 | awk -F"Total: " '{ print $2 }' | sed 's/^[ \t]*//;s/[ \t]*$//'`
-  if [ "$CONSOLE_SHORT" == "" ]; then
+# CZY=`echo "$MINER_PATH" | grep -i "lolminer" | wc -l`
+# if [ "$CZY" == "1" ]; then
+  # CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | sed 's/\r/\n/g' | grep -a "^Average speed" | tail -n 1 | awk -F"Total: " '{ print $2 }' | sed 's/^[ \t]*//;s/[ \t]*$//'`
+  # if [ "$CONSOLE_SHORT" == "" ]; then
     # brak sekcji Total, ktora wystepuje tylko dla count(GPU)>1
-    CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | sed 's/\r/\n/g' | grep -a "^Average speed" | tail -n 1 | awk '{ print $4" "$5 }'`
-  fi
-fi
+    # CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | sed 's/\r/\n/g' | grep -a "^Average speed" | tail -n 1 | awk '{ print $4" "$5 }'`
+  # fi
+# fi
 
 ### xmr
-CZY=`echo "$MINER_PATH" | grep -i "xmr-stak" | wc -l`
-if [ "$CZY" == "1" ]; then
-  screen -S miner -X stuff "h" 1>/dev/null 2>/dev/null; sleep 1
-  CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "Totals (ALL)" | tail -n 1 | awk '{ print $3" "$6 }'`
-fi
+# CZY=`echo "$MINER_PATH" | grep -i "xmr-stak" | wc -l`
+# if [ "$CZY" == "1" ]; then
+  # screen -S miner -X stuff "h" 1>/dev/null 2>/dev/null; sleep 1
+  # CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "Totals (ALL)" | tail -n 1 | awk '{ print $3" "$6 }'`
+# fi
 
 ### xmrig
-CZY=`echo "$MINER_PATH" | grep -i "xmrig" | wc -l`
-[ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "speed" | tail -n 1 | awk '{ print $5" "$8 }'`
+# CZY=`echo "$MINER_PATH" | grep -i "xmrig" | wc -l`
+# [ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "speed" | tail -n 1 | awk '{ print $5" "$8 }'`
 
 ### z-enemy-v1.0.9a|1.10
-CZY=`echo "$MINER_PATH" | grep -i "z-enemy" | wc -l`
-[ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "Shares" | grep -a "H/s" | tail -n 1 | awk '{ print $10 }' | tr -d ","`
+# CZY=`echo "$MINER_PATH" | grep -i "z-enemy" | wc -l`
+# [ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "Shares" | grep -a "H/s" | tail -n 1 | awk '{ print $10 }' | tr -d ","`
 
 ### z-enemy-1.0.8
-CZY=`echo "$MINER_PATH" | grep -i "z-enemy-v1.08" | wc -l`
-[ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "H/s - OK" | tail -n 1 | awk '{ print $10 }'`
+# CZY=`echo "$MINER_PATH" | grep -i "z-enemy-v1.08" | wc -l`
+# [ "$CZY" == "1" ] && CONSOLE_SHORT=`echo "$CONSOLE_SHORT_PRE" | grep -a "H/s - OK" | tail -n 1 | awk '{ print $10 }'`
 
 
 
@@ -136,11 +134,11 @@ CZY=`echo "$MINER_PATH" | grep -i "z-enemy-v1.08" | wc -l`
 # ?
 
 # xmr-stak: wrong vendor driver
-CZY=`echo "$CONSOLE" | grep -ai "Unable to query number of CUDA devices\|does not support CUDA\|driver version is insufficient\|wrong vendor driver" | wc -l`
-if [ "$CZY" -ge 1 ]; then
-  CONSOLE_SHORT="See console"
-  CONSOLE=$CONSOLE"\n<span style='color:red;'><b>!!! Please download newest SimpleMiningOS image in order to use this miner !!!</b></span>"
-fi
+# CZY=`echo "$CONSOLE" | grep -ai "Unable to query number of CUDA devices\|does not support CUDA\|driver version is insufficient\|wrong vendor driver" | wc -l`
+# if [ "$CZY" -ge 1 ]; then
+  # CONSOLE_SHORT="See console"
+  # CONSOLE=$CONSOLE"\n<span style='color:red;'><b>!!! Please download newest SimpleMiningOS image in order to use this miner !!!</b></span>"
+# fi
 
 
 # filter out special characters
@@ -152,3 +150,4 @@ CONSOLE_SHORT=`echo "$CONSOLE_SHORT" | awk '{ print substr($0, 1, 30) }'`
 
 
 echo $CONSOLE_SHORT
+echo $CONSOLE
