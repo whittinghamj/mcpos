28677d3f7d9402942a805e32b4c27b119a5ecdf9
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-10-04 18:29:03 +0100

    no message

--- get_bandwidth.sh
+++ get_bandwidth.sh
@@ -1,15 +1,9 @@
 #/bin/bash
 
-#purpose: this script can be use to get the report of total band
-interface=eth0
-community=public
-interval=60
-server=localhost
-clear
-echo "\033[0m\033[1m\033[5m@@ Network Interfaces Bandwidth Monitor @@\033[0m"
-echo "========================================================="
-echo -n "Finding interfaces $interface instance details..."
-echo
+# echo "\033[0m\033[1m\033[5m@@ Network Interfaces Bandwidth Monitor @@\033[0m"
+# echo "========================================================="
+# echo -n "Finding interfaces $interface instance details..."
+# echo
 
 if [ -z "$1" ]; then
         echo
@@ -22,8 +16,8 @@ fi
 
 IF=$1
 
-while true
-do
+# while true
+# do
         R1=`cat /sys/class/net/$1/statistics/rx_bytes`
         T1=`cat /sys/class/net/$1/statistics/tx_bytes`
         sleep 1
@@ -33,5 +27,5 @@ do
         RBPS=`expr $R2 - $R1`
         TKBPS=`expr $TBPS / 1024`
         RKBPS=`expr $RBPS / 1024`
-        echo "Upload $1: $TKBPS kb/s | Download $1: $RKBPS kb/s"
-done
+        echo "Download: $RKBPS kb/s | Upload: $TKBPS kb/s"
+# done
